3242cf9d4354a847b241cc7ea8cbe0be7a74b52a
CZ729 <3188958453@qq.com>
2026-07-08 21:25:41 +0800

    上传数据结构中顺序表的代码

---
+++ 顺序表/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//顺序表----静态分配
+    //#define MaxSize 10
+    //typedef struct {
+        //int data[MaxSize];
+        //int length;
+    //}SqList;
+
+    //void InitList(SqList *L) {
+        //L->length = 0;
+    //}
+
+    //int main() {
+        //SqList L;
+        //InitList(&L);
+    //}
+
+//顺序表----动态分配
+    //#define InitSize 10
+    //typedef struct {
+        //int *data;
+        //int MaxSize;
+        //int length;
+    //}SeqList;
+
+    //void InitList(SeqList *L) {
+        //L -> data = (int *)malloc(sizeof(int)*InitSize);
+        //L -> length = 0;
+        //L -> MaxSize = InitSize;
+    //}
+
+    //void IncreaseSize(SeqList *L , int len) {
+        //int *p = L -> data;
+        //L -> data = (int *)malloc((L -> MaxSize + len) * sizeof(int));
+        //for (int i = 0; i < len; i++) {
+            //L -> data[i] = *p;
+        //}
+        //L -> MaxSize += len;
+        //free(p);
+    //}
+
+    //int main(void) {
+        //SeqList L;
+        //InitList(&L);
+        //return 0;
+    //}
+
+//顺序表----存入数据 (从尾部存入数据)
+    //bool AddTail(SqList *L,int val) {
+        //if (L -> length >= MaxSize) {
+            //printf("顺序表已满,无法存入/n");
+            //return false;
+        //}
+        //L -> data[L -> length] = val;
+        //L -> length++;
+        //return true;
+    //}
+    //后续调用,如:AddTail(&L,10);
+    //用cout 输出调用函数的代码来得到代码是否成功运行,成功输出1,失败输出0
+
+//顺序表----插入数据 (在L中位序i处插入元素e)
+    //bool ListInsert(SqList *L,int i,int e) {
+        //if (i < 1 || i > L -> length+1) {
+            //return false;
+        //}
+        //if (L -> length >= MaxSize) {
+            //return false;
+        //}
+        //for (int j = L->length; j >= i; j--) {  (将第i个元素及以后的元素后移)
+            //L->data[j] = L->data[j-1];
+        //}
+        //L->data[i-1] = e; (在位置i放入e)
+        //L->length++; (长度加1)
+        //return true;
+    //}
+    //用cout输出调用函数的代码,成功输出1,失败输出0
+
+//顺序表----删除数据 (删除在L中位序为i的元素,并用e返回删除元素的值)
+    //bool ListDelete(Sqlist *L,int i,int *e) { (判断i的范围是否有效)
+        //if (i < 1 || i > L -> length+1) {
+            //return false;
+        //}
+        //e = L -> data[i-1]; (将被删除的元素赋值给e)
+        //for (int j = i; j < L -> length; j++) { (将第i个位置后的元素前移)
+            //L -> data[j-1] = L -> data[j];
+        //}
+        //L -> length--; (线性表长度见1)
+        //return true;
+    //}
+    //使用前要先定义一个同类型变量e
