79f00b3788adc088cebd2f883aea89a1a14f386d
CZ729 <3188958453@qq.com>
2026-07-14 21:23:12 +0800

    链栈操作代码

---
+++ 链栈/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//链栈的定义
+typedef struct Linknode {
+    int data; //数据域
+    struct Linknode *next; //指针域
+}LiStack; //栈类型定义
+
+//链栈的初始化
+bool InitStack(LiStack *s) {
+    if (s == NULL) {
+        return false;
+    }
+    s->next = NULL;
+    return true;
+}
+
+//判断栈空
+bool StackEmpty(LiStack *s) {
+    if (s->next == NULL) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+//压栈操作
+bool Push(LiStack *s, int x) {
+    LiStack *P = (LiStack *)malloc(sizeof(LiStack));
+    if (P == NULL) {
+        return false;
+    }
+    P->data = x;
+    P->next = s->next;
+    s->next = P;
+    return true;
+}
+
+//出栈操作
+bool Pop(LiStack *s, int *x) {
+    if (s == NULL) {
+        return false;
+    }
+    LiStack *p = s->next;
+    *x = p->data;
+    s->next = p->next;
+    free(p);
+    return true;
+}
+
+//读取栈顶元素
+bool GetTop(LiStack *s, int *x) {
+    if (s == NULL) {
+        return false;
+    }
+    *x = s->data;
+    return true;
+}
+
+int main(void) {
+    LiStack s;
+    InitStack(&s);
+    return 0;
+}
